e12885d9c761e128bc4c4af9756363bcb946b21d
satyamrai17 <satyamrai8419@gmail.com>
2024-01-10 01:25:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Join from "./component/Join/Join";
+import Chat from "./component/Chat/Chat"; // Import the Chat component
+import Message from './component/Message/Message';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Join/>}/>
+          <Route path="/chat" element={<Chat/>}/>
+          <Route path='/message' element={<Message/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
